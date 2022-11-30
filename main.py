0dea5874edcb513a2fc056700f51e7dc3ab3b290
Home <yanama758@gmail. com>
2022-11-30 21:48:06 +0200

    Початок коду

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello.\n My name is Yana.\n Its my dog JASPER.\n Today he:")
+class Dog:
+    def __init__(self, nine, ten, eleven, fifteen, seventeen, twenty, twenty_two):
+        self.nine = nine
+        self.ten = ten
+        self.eleven = eleven
+        self.fifteen = fifteen
+        self.seventeen = seventeen
+        self.twenty = twenty
+        self.twenty_two = twenty_two
+
+d1 = Dog("nine o'clock - eat\n", "ten o'clock - start a walk\n", "eleven o'clock - finish a walk\n", "fifteen o'clock - sleep\n", "seventeen o'clock - playing\n", "twenty o'clock - a walk\n", "twenty_two o'clock - eat and sleep")
